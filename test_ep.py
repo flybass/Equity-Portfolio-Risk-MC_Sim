0a3243226d6a6cc2ae689e69cdaeafec2525409f
Daniel Abramson <flybass@gmail.com>
2015-01-19 22:18:13 -0500

    Update test_ep.py

--- test_ep.py
+++ test_ep.py
@@ -10,7 +10,7 @@ port_d = dict(zip(some_tickers, some_amts))
 portfolio = ep_risk(port_d)
 
 #example calls
-print "(Var,ES) under alpha=1"
+print "(Var,ES) under alpha=1%"
 print "1. PCA Approach: ", portfolio.pca_risk(1, n_days=2, end_date = "01/15/15", window_length = 300)
 print "2. Uncleaned Corr: ",portfolio.unclean_risk(1, n_days=2, end_date = "01/15/15", window_length = 300)
 print "3. Historical Risk", portfolio.historical_risk(1, n_days=2, end_date = "01/15/15", window_length = 300)
